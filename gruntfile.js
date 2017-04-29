08d56de84e24e3f92ddb4db0572a3dc1af67013a
Kaja <ayreeve@wp.pl>
2017-04-29 18:34:53 +0200

    gruntfile update

--- gruntfile.js
+++ gruntfile.js
@@ -13,33 +13,34 @@ module.exports = function (grunt) {
             }
         },
 
-        watch: {
-            scripts: {
-                files: ['sass/*.sass'],
-                tasks: ['sass'],
-                options: {
-                    spawn: false,
-                },
-            }
-
-        },
-
         browserSync: {
             bsFiles: {
-                src: 'css/*.css'
+                src: 'sass/*.sass'
             },
             options: {
                 server: {
                     baseDir: "./"
                 }
             }
+        },
+
+        watch: {
+            scripts: {
+                files: ['sass/*.sass'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+
         }
+
     });
 
     // Load the plugins tasks
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s)
     grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
